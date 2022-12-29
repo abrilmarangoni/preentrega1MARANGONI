2e5c2ee2872cf03f22c5052678ec82359fbe9789
abril <marangonii.abril@gmail.com>
2022-12-29 13:06:08 -0300

    prueba

---
+++ index.js
@@ -0,0 +1,22 @@
+//comentarios de una linea con // nos sirve para ver si el archivo esta linkeado
+console.log("mi primera linea bien usada");
+
+//declaracion de espacio de memorias
+//forma 1 con var para variables: es una practica  no recomendada usar var es mejor let
+//declarando una variable
+// var autor;
+//autor = "marangoni abril";
+// console.log(autor);   trae problemas este caso
+
+//en una variable tenemos su nombre y valor
+//buena practica
+let autora = "departamento";
+autora = "semipiso";
+console.log(autora);
+//una variable es un espacio de memoria donde puedo usar un valor y despues reutilizarlo
+//js es sensible a la mayus y a la minus console.log AUTORA O Autora no sirven xq esta distinto
+
+//tercera forma asignar espacio de memoria mediante const
+const edificio = "palcos del golf";
+console.log(edificio);
+//a unna constante no le puedo reasignar otro valor
